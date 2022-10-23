2f5d628796d94fe457ed07944918ba538116b425
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-10-23 16:28:29 +0530

    build feedback form

--- feedback/lib/Screens/feedback.dart
+++ feedback/lib/Screens/feedback.dart
@@ -48,6 +48,33 @@ class feedback extends StatelessWidget {
             buildCheckItem("Personal profile"),
             buildCheckItem("Other issues"),
             buildCheckItem("Suggestions"),
+            SizedBox(
+              height: 20.0,
+            ),
+            buildFeedbackForm(),
+            SizedBox(
+              height: 20.0,
+            ),
+            buildNumberField(),
+            Spacer(),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: flatButton(
+                    onpressed: () {},
+                    color: Color(0xffe5e5e5),
+                    Padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "SUBMIT",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
@@ -55,9 +82,36 @@ class feedback extends StatelessWidget {
   }
 }
 
+buildFeedbackForm() {
+  return Container(
+    height: 200.0,
+    child: Stack(
+      children: <Widget>[
+        TextField(
+          maxLines: 10,
+        ),
+      ],
+    ),
+  );
+}
+
 buildCheckItem(title) {
   return Padding(
     padding: EdgeInsets.only(bottom: 15.0),
-    child: Row(),
+    child: Row(
+      children: <Widget>[
+        Icon(
+          Icons.check_circle,
+          color: Colors.blue,
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        )
+      ],
+    ),
   );
 }
